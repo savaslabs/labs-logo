512cc6f83de82dabe43ab87a3bcc85f010e92231
maddybvh <maddybvh@gmail.com>
2020-08-21 14:55:05 -0400

    Updates

---
+++ precache-manifest.a1c59397b3867e930b5dc1700af5717c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ba63d57c34c71c739f983d82b09300",
+    "url": "/labs-logo/index.html"
+  },
+  {
+    "revision": "e61a95398f0232e3bd3a",
+    "url": "/labs-logo/static/css/main.d44b62bc.chunk.css"
+  },
+  {
+    "revision": "e117d24d36b259c508da",
+    "url": "/labs-logo/static/js/2.1e64eec3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/labs-logo/static/js/2.1e64eec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61a95398f0232e3bd3a",
+    "url": "/labs-logo/static/js/main.b4b1f11b.chunk.js"
+  },
+  {
+    "revision": "69c068f589b0d09e35b1",
+    "url": "/labs-logo/static/js/runtime-main.a6d055e1.js"
+  }
+]);
